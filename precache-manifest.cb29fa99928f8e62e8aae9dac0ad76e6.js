d83d3419012e2f80d5da6d549076b255ec6225a1
pyroplushy <92647431+pyroplushy@users.noreply.github.com>
2023-09-13 19:13:59 -0400

    Updates

---
+++ precache-manifest.cb29fa99928f8e62e8aae9dac0ad76e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4292fa8eaecc99846ef845a567a905ae",
+    "url": "/SpaceX-Launches-App/index.html"
+  },
+  {
+    "revision": "ef3b71968ede6c87a4ef",
+    "url": "/SpaceX-Launches-App/static/css/main.a3ef3333.chunk.css"
+  },
+  {
+    "revision": "2dfc35768f6be961cab6",
+    "url": "/SpaceX-Launches-App/static/js/2.8a63c904.chunk.js"
+  },
+  {
+    "revision": "ef3b71968ede6c87a4ef",
+    "url": "/SpaceX-Launches-App/static/js/main.78167b43.chunk.js"
+  },
+  {
+    "revision": "d12f9907e449d343ed40",
+    "url": "/SpaceX-Launches-App/static/js/runtime~main.96728327.js"
+  }
+]);
